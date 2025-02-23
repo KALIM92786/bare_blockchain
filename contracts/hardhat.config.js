997152d91d7e19599ecbfdcf5e53d58a58805edf
kalim ansari <kalim199919@gmail.com>
2025-02-23 16:41:50 +0530

    Organized project structure

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,7 +3,4 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  paths: {
-    sources: "./src"  // Only compile contracts in the src folder
-  },
 };
